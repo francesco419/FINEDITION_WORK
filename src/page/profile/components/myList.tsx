efa8854731c3fdd6462e31a5f5c78e93d92bed16
Lee Sang Hyeon <francesco419@naver.com>
2023-10-20 19:42:47 +0900

    231020 4th

--- src/page/profile/components/myList.tsx
+++ src/page/profile/components/myList.tsx
@@ -1,26 +1,42 @@
 import Card from '../../../components/card/cardComp';
 import { cardData } from '../../../temp/cardData';
 import _ from 'lodash';
+import { useAppSelector } from '../../../redux/hooks';
+import { selectView } from '../../../redux/slices/viewSlice';
+import { storyCardData } from '../../../data/storyCardData';
+import { selectBookmark } from '../../../redux/slices/bookmarklikeSlice';
+import { useNavigate } from 'react-router';
 
 export default function ProfileMyList() {
+  const view = useAppSelector(selectView);
+  const bookmark = useAppSelector(selectBookmark);
+  const nav = useNavigate();
+
+  const toBookmark = () => {
+    nav('/bookmark');
+  };
+
   return (
     <div className='profileMyList'>
       <div className='profileMyList_header'>
-        <p>00 Viewed</p>
-        <p>00 Bookmarked*</p>
+        <p>{view.view.length} Viewed</p>
+        <button onClick={toBookmark}>
+          <p>{bookmark.bookmark.length} Bookmarked*</p>
+        </button>
       </div>
-      <ul className='profileMyList_list'>
-        {_.map(_.shuffle(cardData), (o, index) => {
-          if (index > 40) {
-            return;
-          }
-          return (
-            <li>
-              <Card data={o} color={'#fff'} type='small' fcolor='#000' />
-            </li>
-          );
-        })}
-      </ul>
+      <div className='profileMyList_list'>
+        <ul>
+          {_.map(_.shuffle([...cardData, ...storyCardData]), (o, index) => {
+            if (view.view.includes(o.id)) {
+              return (
+                <li>
+                  <Card data={o} color={'#fff'} type='small' fcolor='#000' />
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
